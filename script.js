e0eaa49f7f2544739e969f0c6cabcf76aac36886
Luke Reed <luke.reed@outlook.com.au>
2023-01-17 11:53:00 +1100

    Made more readable

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const choices = ["rock", "paper", "scissors"];
 function playRound() {
 	// Random computer choice
 	let computerSelection = choices[Math.floor(Math.random() * choices.length)];
-	// Asks player for their choice and converts to lowercase. If user enters anything outside of the choices it asks them again. Quits if escape is pressed or user cancels
+	
+	// Asks player for their choice and converts to lowercase. If user enters anything outside of the choices it re-asks.
 	let playerSelection = prompt("Rock, paper or scissors?");
 	if (playerSelection === null) {
 		alert("Game quit!");
@@ -16,12 +17,17 @@ function playRound() {
 			return "No result, the game was quit.";
 		}
 	}
+
 	playerSelection = playerSelection.toLowerCase();
 	// Compares player and computer choices and outputs accordingly
 	if (playerSelection === computerSelection) {
 		return `It's a tie! You both chose ${playerSelection}.`;
-	} else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
+
+	} else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+	 		   (playerSelection === "scissors" && computerSelection === "paper") ||
+			   (playerSelection === "paper" && computerSelection === "rock")) {
 		return `You win! ${playerSelection} beats ${computerSelection}.`;
+
 	} else {
 		return `You lose! ${computerSelection} beats ${playerSelection}.`;
 	}
