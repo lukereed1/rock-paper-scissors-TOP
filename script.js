5f3fca0b296e5b0c6b7519df5257881b05dbe4f2
Luke Reed <luke.reed@outlook.com.au>
2023-01-17 14:41:33 +1100

    Added a 'game' function that:
    - Creates a game consisting of 5 rounds
    - Calls playRound function each round and outputs the results in the console
    - After the five rounds the scores are displayed along with the winner
    
    Also added two new variables to keep track of player/computer score. Incrementing mentioned variables in the conditional statement where the winner of each round is decided in the playRound function

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
 const choices = ["rock", "paper", "scissors"];
+let playerSelection = " ";
+let computerSelection = " ";
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound() {
 	// Random computer choice
-	let computerSelection = choices[Math.floor(Math.random() * choices.length)];
-	
-	// Asks player for their choice and converts to lowercase. If user enters anything outside of the choices it re-asks.
-	let playerSelection = prompt("Rock, paper or scissors?");
+	computerSelection = choices[Math.floor(Math.random() * choices.length)];
+
+	// Asks player for their choice and converts to lowercase. If user enters anything outside of the choices it re-asks
+	playerSelection = prompt("Rock, paper or scissors?");
 	if (playerSelection === null) {
-		alert("Game quit!");
-		return "No result, the game was quit.";
+		playerSelection = "Nothing!";
+		alert("Round quit");
+		return "No result, the round was quit";
 	}
 	while (!choices.includes(playerSelection.toLowerCase())) {
 		playerSelection = prompt("Please enter rock, paper or scissors. Also check spelling");
 		if (playerSelection === null) {
-			alert("Game quit!");
-			return "No result, the game was quit.";
+			playerSelection = "Nothing!";
+			alert("Round quit");
+			return "No result, the round was quit";
 		}
 	}
 	playerSelection = playerSelection.toLowerCase();
-	
+
 	// Compares player and computer choices and outputs accordingly
 	if (playerSelection === computerSelection) {
 		return `It's a tie! You both chose ${playerSelection}.`;
-
-	} else if ((playerSelection === "rock" && computerSelection === "scissors") ||
-	 	(playerSelection === "scissors" && computerSelection === "paper") ||
-		(playerSelection === "paper" && computerSelection === "rock")) {
+	} else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
+		++playerScore;
 		return `You win! ${playerSelection} beats ${computerSelection}.`;
-
 	} else {
+		++computerScore;
 		return `You lose! ${computerSelection} beats ${playerSelection}.`;
 	}
 }
-console.log(playRound());
+
+//Starts a five round game, giving both choices and a score update each round and a final result at the end
+function game() {
+	for (i = 1; i <= 5; ++i) {
+		let result = playRound();
+		console.log(`Round ${i}: `);
+		console.log(`Player chose: ${playerSelection}`);
+		console.log(`Computer chose: ${computerSelection}`);
+		console.log(result);
+		console.log(`Player score: ${playerScore}`);
+		console.log(`computerScore: ${computerScore}`);
+		console.log("----------------------------------");
+	}
+
+	console.log(`Final score:`);
+	console.log(`Player: ${playerScore}`);
+	console.log(`Computer: ${computerScore}`);
+
+	if (playerScore === computerScore) {
+		console.log("It's a draw! Try again");
+	} else if (playerScore > computerScore) {
+		console.log("You win! Nice job");
+	} else {
+		console.log("You lost! Better luck next time");
+	}
+}
+
+game();
