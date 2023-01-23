d441e40e86c06ffa9393677655dce89ef2c72f0a
Luke Reed <luke.reed@outlook.com.au>
2023-01-24 09:00:04 +1100

    Tidied up script and some minor css changes

--- script.js
+++ script.js
@@ -9,46 +9,10 @@ let compScore = document.querySelector("#computer-score");
 const resultList = document.createElement("ul");
 body.appendChild(resultList);
 
+// User buttons to play
 userRock.addEventListener("click", playRound);
 userPaper.addEventListener("click", playRound);
 userScissors.addEventListener("click", playRound);
-
-// Generates random choice for computer
-function computerRandom() {
-	let random = Math.floor(Math.random() * 3);
-	switch (random) {
-		case 0:
-			return "rock";
-			break;
-		case 1:
-			return "paper";
-			break;
-		case 2:
-			return "scissors";
-	}
-}
-
-// Updates scoreboard
-const userWin = () => (userScore.innerText = Number(userScore.innerText) + 1);
-const computerWin = () => (compScore.innerText = Number(compScore.innerText) + 1);
-
-if (userScore.innerText >= 5) {
-	listItem.classList.add("win-result");
-	listItem.appendChild(document.createTextNode("Congratulations, you've won!"));
-	resultList.insertBefore(listItem, resultList.firstChild);
-	body.insertBefore(playAgainBtn, resultList);
-	userRock.removeEventListener("click", playRound);
-	userPaper.removeEventListener("click", playRound);
-	userScissors.removeEventListener("click", playRound);
-} else if (compScore.innerText >= 5) {
-	listItem.classList.add("lose-result");
-	listItem.appendChild(document.createTextNode("Unlucky, you lost!"));
-	resultList.insertBefore(listItem, resultList.firstChild);
-	body.insertBefore(playAgainBtn, resultList);
-	userRock.removeEventListener("click", playRound);
-	userPaper.removeEventListener("click", playRound);
-	userScissors.removeEventListener("click", playRound);
-}
 
 function playRound(e) {
 	const playerSelection = e.target.id;
@@ -57,10 +21,10 @@ function playRound(e) {
 	const winResult = document.createElement("li");
 	const loseResult = document.createElement("li");
 	const playAgainBtn = document.createElement("button");
-	playAgainBtn.addEventListener("click", refreshPage);
+	playAgainBtn.addEventListener("click", refreshPage); // Restarts game when button clicked
 	playAgainBtn.textContent = "Play again?";
 	playAgainBtn.classList.add("play-again-btn");
-	listItem.classList.add("result-list");
+	listItem.classList.add("result-list"); // Styles round result list
 	if (
 		(playerSelection === "rock" && computerSelection === "scissors") ||
 		(playerSelection === "scissors" && computerSelection === "paper") ||
@@ -68,9 +32,9 @@ function playRound(e) {
 	) {
 		listItem.appendChild(
 			document.createTextNode(`You win, ${playerSelection} beats ${computerSelection}!`)
-		);
-		resultList.insertBefore(listItem, resultList.firstChild);
-		userWin();
+		); // Adds round result to list
+		resultList.insertBefore(listItem, resultList.firstChild); // Ensures round result goes to top of list
+		userWin(); // Adds a point to the scoreboard
 		if (userScore.innerText >= 5) {
 			winResult.classList.add("win-result");
 			winResult.appendChild(document.createTextNode("Congratulations, you've won!"));
@@ -103,6 +67,21 @@ function playRound(e) {
 	}
 }
 
+// Generates random choice for computer
+function computerRandom() {
+	let random = Math.floor(Math.random() * 3);
+	switch (random) {
+		case 0:
+			return "rock";
+			break;
+		case 1:
+			return "paper";
+			break;
+		case 2:
+			return "scissors";
+	}
+}
+
 function disableUserButtons() {
 	userRock.removeEventListener("click", playRound);
 	userPaper.removeEventListener("click", playRound);
@@ -111,4 +90,12 @@ function disableUserButtons() {
 
 function refreshPage() {
 	window.location.reload(true);
+}
+
+function userWin() {
+	userScore.innerText = Number(userScore.innerText) + 1;
+}
+
+function computerWin() {
+	compScore.innerText = Number(compScore.innerText) + 1;
 }
