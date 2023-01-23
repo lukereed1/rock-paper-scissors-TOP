110266e45b5770f689d120d028fd4b86f772a15f
Luke Reed <luke.reed@outlook.com.au>
2023-01-23 13:38:31 +1100

    Basic UI complete and started adding DOM events

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## General Info
 
-A console game of rock paper scissors, best of five rounds. With the user going against the computer.
+This project is a simple implementation of the classic game "Rock, Paper, Scissors" with a user interface. The user can play against the computer by selecting one of the three options (rock, paper, or scissors) and the computer will make a random selection. The winner of the round will be determined based on the standard rules of the game and the score will be displayed on the screen. Developed with HTML, CSS and Javascript.
 
 ## Goals
 
-To practice using functions and conditionals. Also to get the hang of problem solving and breaking the task down into individual parts.
+The goal of this project is to develop a better understanding of Javascript, mainly focusing on functions, conditionals and DOM manipulation. As well as practicing a bit of HTML and CSS.
--- script.js
+++ script.js
@@ -1,72 +1,57 @@
 const choices = ["rock", "paper", "scissors"];
-let playerSelection = " ";
-let computerSelection = " ";
-let playerScore = 0;
-let computerScore = 0;
 
-function playRound() {
-	// Random computer choice
-	computerSelection = choices[Math.floor(Math.random() * choices.length)];
-
-	// Asks player for their choice and converts to lowercase. If user enters anything outside of the choices it re-asks
-	playerSelection = prompt("Rock, paper or scissors?");
-	if (playerSelection === null) {
-		playerSelection = "Nothing!";
-		alert("Round quit");
-		return "No result, the round was quit";
+function computerRandom() {
+	let random = Math.floor(Math.random() * 3);
+	switch (random) {
+		case 0:
+			return "rock";
+			break;
+		case 1:
+			return "paper";
+			break;
+		case 2:
+			return "scissors";
 	}
-	while (!choices.includes(playerSelection.toLowerCase())) {
-		playerSelection = prompt(
-			"Please enter rock, paper or scissors. Also check spelling"
-		);
-		if (playerSelection === null) {
-			playerSelection = "Nothing!";
-			alert("Round quit");
-			return "No result, the round was quit";
-		}
-	}
-	playerSelection = playerSelection.toLowerCase();
+}
 
-	// Compares player and computer choices and outputs accordingly
+const body = document.querySelector("body");
+const userRock = document.querySelector("#rock");
+const userPaper = document.querySelector("#paper");
+const userScissors = document.querySelector("#scissors");
+const resultList = document.createElement("ul");
+
+userRock.addEventListener("click", playRound);
+userPaper.addEventListener("click", playRound);
+userScissors.addEventListener("click", playRound);
+
+body.appendChild(resultList);
+
+function playRound(e) {
+	const playerSelection = e.target.id;
+	const computerSelection = computerRandom();
+	const listItem = document.createElement("li");
 	if (playerSelection === computerSelection) {
-		return `It's a tie! You both chose ${playerSelection}.`;
+		listItem.appendChild(
+			document.createTextNode(`It's a draw, you both chose ${playerSelection}!`)
+		);
+		resultList.appendChild(listItem);
 	} else if (
 		(playerSelection === "rock" && computerSelection === "scissors") ||
 		(playerSelection === "scissors" && computerSelection === "paper") ||
 		(playerSelection === "paper" && computerSelection === "rock")
 	) {
-		++playerScore;
-		return `You win! ${playerSelection} beats ${computerSelection}.`;
+		listItem.appendChild(
+			document.createTextNode(
+				`You win, ${playerSelection} beats ${computerSelection}!`
+			)
+		);
+		resultList.appendChild(listItem);
 	} else {
-		++computerScore;
-		return `You lose! ${computerSelection} beats ${playerSelection}.`;
+		listItem.appendChild(
+			document.createTextNode(
+				`You lose, ${computerSelection} beats ${playerSelection}!`
+			)
+		);
+		resultList.appendChild(listItem);
 	}
 }
-
-// Starts a five round game, giving both choices and a score update each round and a final result at the end
-function game() {
-	for (i = 1; i <= 5; ++i) {
-		let result = playRound();
-		console.log(`Round ${i}: `);
-		console.log(`Player chose: ${playerSelection}`);
-		console.log(`Computer chose: ${computerSelection}`);
-		console.log(result);
-		console.log(`Player score: ${playerScore}`);
-		console.log(`computerScore: ${computerScore}`);
-		console.log("----------------------------------");
-	}
-
-	console.log(`Final score:`);
-	console.log(`Player: ${playerScore}`);
-	console.log(`Computer: ${computerScore}`);
-
-	if (playerScore === computerScore) {
-		console.log("It's a draw! Try again");
-	} else if (playerScore > computerScore) {
-		console.log("You win! Nice job");
-	} else {
-		console.log("You lost! Better luck next time");
-	}
-}
-
-game();
