28fcb89aab9f2937beb7ece6dad03f310c2ff134
Luke Reed <luke.reed@outlook.com.au>
2023-01-24 13:22:20 +1100

    Tidied up script to make more readable, and: - A black border now shows around the users and computers choice each round - An error message is show if the user tries to click the computers buttons - Buttons don't work on either side once the game is over - Created functions to remove border around either sides picks before a new round is started

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
+// Naming buttons to add events
 const body = document.querySelector("body");
 const userRock = document.querySelector("#rock");
 const userPaper = document.querySelector("#paper");
 const userScissors = document.querySelector("#scissors");
+const computerRock = document.querySelector("#computer-rock");
+const computerPaper = document.querySelector("#computer-paper");
+const computerScissors = document.querySelector("#computer-scissors");
 let userScore = document.querySelector("#user-score");
 let compScore = document.querySelector("#computer-score");
 
-// List created and added to body, used to show round results
-const resultList = document.createElement("ul");
-body.appendChild(resultList);
-
-// User buttons to play
+// User buttons that start each round
 userRock.addEventListener("click", playRound);
 userPaper.addEventListener("click", playRound);
 userScissors.addEventListener("click", playRound);
+
+// Display an error message if the user clicks a button on the computers side
+computerRock.addEventListener("click", computerMessage);
+computerPaper.addEventListener("click", computerMessage);
+computerScissors.addEventListener("click", computerMessage);
+
+// List created and added to body which is used to show round results
+const resultList = document.createElement("ul");
+body.appendChild(resultList);
 
 function playRound(e) {
 	const playerSelection = e.target.id;
@@ -24,6 +33,9 @@ function playRound(e) {
 	playAgainBtn.addEventListener("click", refreshPage); // Restarts game when button clicked
 	playAgainBtn.textContent = "Play again?";
 	playAgainBtn.classList.add("play-again-btn");
+	removeUsersLastPick(); // Removes border from previous rounds choice
+	removeCompsLastPick();
+	this.classList.add("user-comp-choice"); // Puts a border around the users choice
 	listItem.classList.add("result-list"); // Styles round result list
 	if (
 		(playerSelection === "rock" && computerSelection === "scissors") ||
@@ -33,14 +45,14 @@ function playRound(e) {
 		listItem.appendChild(
 			document.createTextNode(`You win, ${playerSelection} beats ${computerSelection}!`)
 		); // Adds round result to list
-		resultList.insertBefore(listItem, resultList.firstChild); // Ensures round result goes to top of list
+		resultList.insertBefore(listItem, resultList.firstChild); // Ensures round result goes to top of list and not the bottom
 		userWin(); // Adds a point to the scoreboard
 		if (userScore.innerText >= 5) {
 			winResult.classList.add("win-result");
 			winResult.appendChild(document.createTextNode("Congratulations, you've won!"));
 			resultList.insertBefore(winResult, resultList.firstChild);
 			body.insertBefore(playAgainBtn, resultList);
-			disableUserButtons();
+			disableAllButtons();
 		}
 	} else if (
 		(playerSelection === "rock" && computerSelection === "paper") ||
@@ -57,7 +69,7 @@ function playRound(e) {
 			loseResult.appendChild(document.createTextNode("Unlucky, you lost!"));
 			resultList.insertBefore(loseResult, resultList.firstChild);
 			body.insertBefore(playAgainBtn, resultList);
-			disableUserButtons();
+			disableAllButtons();
 		}
 	} else {
 		listItem.appendChild(
@@ -65,6 +77,7 @@ function playRound(e) {
 		);
 		resultList.insertBefore(listItem, resultList.firstChild);
 	}
+	computerAnimation(computerSelection); // Highlights whatever choice was chosen for the computer
 }
 
 // Generates random choice for computer
@@ -81,21 +94,55 @@ function computerRandom() {
 			return "scissors";
 	}
 }
-
-function disableUserButtons() {
-	userRock.removeEventListener("click", playRound);
-	userPaper.removeEventListener("click", playRound);
-	userScissors.removeEventListener("click", playRound);
+// Puts a black border around whatever choice is randomly generated for the computer
+function computerAnimation(computerRandom) {
+	if (computerRandom === "rock") {
+		computerRock.classList.add("user-comp-choice");
+	} else if (computerRandom === "paper") {
+		computerPaper.classList.add("user-comp-choice");
+	} else if (computerRandom === "scissors") {
+		computerScissors.classList.add("user-comp-choice");
+	}
 }
-
+// Refresh function for the play again button
 function refreshPage() {
 	window.location.reload(true);
 }
-
+// Adds a point to the users score
 function userWin() {
 	userScore.innerText = Number(userScore.innerText) + 1;
 }
-
+// Adds a point to the computers score
 function computerWin() {
 	compScore.innerText = Number(compScore.innerText) + 1;
+}
+// Removes the styling of the users last pick so the new one can be shown
+function removeUsersLastPick() {
+	userRock.classList.remove("user-comp-choice");
+	userPaper.classList.remove("user-comp-choice");
+	userScissors.classList.remove("user-comp-choice");
+}
+// Removes the styling of the computers last pick so the new one can be shown
+function removeCompsLastPick() {
+	computerRock.classList.remove("user-comp-choice");
+	computerPaper.classList.remove("user-comp-choice");
+	computerScissors.classList.remove("user-comp-choice");
+}
+// Error message if user clicks wrong buttons
+function computerMessage() {
+	const errorMessage = document.createElement("li");
+	errorMessage.classList.add("result-list");
+	errorMessage.appendChild(
+		document.createTextNode("You're not a computer, try the buttons on the other side")
+	);
+	resultList.insertBefore(errorMessage, resultList.firstChild);
+}
+// Disables all buttons when the game is over
+function disableAllButtons() {
+	userRock.removeEventListener("click", playRound);
+	userPaper.removeEventListener("click", playRound);
+	userScissors.removeEventListener("click", playRound);
+	computerRock.removeEventListener("click", computerMessage);
+	computerPaper.removeEventListener("click", computerMessage);
+	computerScissors.removeEventListener("click", computerMessage);
 }
